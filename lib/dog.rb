eaa5025d95ef026a6fc374ebdd5630c55347e0cc
DAZUN XING <dazun.xing@gmail.com>
2020-05-16 04:48:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+
+
+class Dog 
+  @@all =[]
+  # @@name =[]
+  attr_reader :name
+  
+  def initialize(name=nil)
+    @name =name
+     self.save
+    # @@name << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.uniq.each do |x|
+      puts x.name
+  
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+   
+  
+end
